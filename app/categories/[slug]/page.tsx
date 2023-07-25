15821c0eb19e5dc70ef2b35bee371c37e83f643e
Philani Ncube <ncbphi001@gmail.com>
2023-07-25 21:04:21 +0200

    conditionally render products

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,26 +3,38 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { getCategoryBySlug } from "@/lib/categories";
 import { getProductsByCategoryBySlug } from "@/lib/products";
+import { Database } from "@/schema";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 const page = async ({params: {slug}}:{params:{slug:string}}) => {
 
+  const supabase = createServerComponentClient<Database>({ cookies });
 
-const categoryData =  getCategoryBySlug(slug);
+const {data:category, error:category_error} = await supabase.from("categories").select("*").eq("slug", slug).single();
+
+const {data:products, error:products_error} = await supabase.from("products").select("*").eq("category_id.slug", slug );
+
 
 const productsData =  getProductsByCategoryBySlug(slug);
 
-const [category, products] = await Promise.all([categoryData, productsData])
+
 
   return (
     <main className="container my-6">
-      <h1 className="text-2xl md:text-4xl font-medium">{category.name}</h1>
+      <h1 className="text-2xl md:text-4xl font-medium">{category?.name}</h1>
       <Separator className="my-4" />
-      {products.length === 0 ? (
+      {products?.length === 0 || !products || products_error ? (
         <div className="flex w-full justify-between items-center">
           <p className="text-xl">No products found</p>
           <Link href="/">
-            <Button className="bg-amber-600 text-black hover:text-amber-600" type="button">Back to Homepage</Button>
+            <Button
+              className="bg-amber-600 text-black hover:text-amber-600"
+              type="button"
+            >
+              Back to Homepage
+            </Button>
           </Link>
         </div>
       ) : (
