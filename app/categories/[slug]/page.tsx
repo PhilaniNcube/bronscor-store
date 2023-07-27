f5f7df5e62c3b4999fc7da3c3b7b792cee053b12
Philani Ncube <ncbphi001@gmail.com>
2023-07-27 10:23:01 +0200

    fetch products

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -12,21 +12,21 @@ const page = async ({params: {slug}}:{params:{slug:string}}) => {
 
 const {data:category, error:category_error} = await supabase.from("categories").select("*").eq("slug", slug).single();
 
-const {data:products, error:products_error} = await supabase.from("products").select("*").eq("category_id.slug", slug );
+const {data:products, error:products_error} = await supabase.from("products").select("*, category_id(*)").eq("category_id", category?.id );
 
 
 
 
   return (
     <main className="container my-6">
-      <h1 className="text-2xl md:text-4xl font-medium">{category?.name}</h1>
+      <h1 className="text-2xl font-medium md:text-4xl">{category?.name}</h1>
       <Separator className="my-4" />
       {products?.length === 0 || !products || products_error ? (
-        <div className="flex w-full justify-between items-center">
+        <div className="flex items-center justify-between w-full">
           <p className="text-xl">No products found</p>
           <Link href="/">
             <Button
-              className="bg-amber-600 text-black hover:text-amber-600"
+              className="text-black bg-amber-600 hover:text-amber-600"
               type="button"
             >
               Back to Homepage
@@ -34,7 +34,7 @@ const {data:products, error:products_error} = await supabase.from("products").se
           </Link>
         </div>
       ) : (
-        <div className="my-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
+        <div className="grid grid-cols-1 gap-4 my-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-8">
           {products.map((product) => (
             <ProductItem key={product.id} product={product} />
           ))}
