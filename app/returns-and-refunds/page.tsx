b13c0104905cd9187c73a2800c1474704e6eb0c6
Philani Ncube <ncbphi001@gmail.com>
2023-11-21 09:07:27 +0200

    Fix bug in login functionality

---
+++ app/returns-and-refunds/page.tsx
@@ -0,0 +1,184 @@
+const ReturnPolicyPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Return Policy</h1>
+      <p className="mt-4">
+        We want you to be happy with your purchase. If you are not completely
+        satisfied, you can return the product to us and we will either
+        repair/replace it, or credit your account, subject to the below terms.
+        This Policy applies to products bought from Bronscor. Please note that
+        Bronscor may, from time to time and at its discretion, restrict the use
+        of the credit in your account in respect of certain products.
+      </p>
+      <p className="mt-4">
+        This Policy forms part of the Bronscor Terms and Conditions, and so
+        words defined in the Terms and Conditions have the same meaning in this
+        Policy, unless the context indicates otherwise. Nothing in this Policy
+        is intended to limit your statutory rights in any way.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8">
+        Preparing your products for a return
+      </h2>
+      <p className="mt-4">
+        To ensure your request is processed as quickly as possible you are
+        responsible for the following when returning your products:
+      </p>
+      <ul className="list-disc ml-8 mt-2">
+        <li>
+          Package your products safely and securely for protection during
+          transit
+        </li>
+        <li>
+          Clearly mark your return reference number on the outside of the parcel
+        </li>
+        <li>
+          Include all accessories and parts that were sold with the product
+        </li>
+      </ul>
+      <p className="mt-4">
+        Failure to adhere to any of these requirements could delay the
+        processing of your request or result in its decline altogether.
+      </p>
+
+      <h3 className="text-lg font-bold mt-8">1: Unwanted products</h3>
+      <p className="mt-4">
+        You can return an unwanted product to us at no charge, provided:
+      </p>
+      <ul className="list-disc ml-8 mt-2">
+        <li>
+          It is undamaged and unused, with the original labels and stickers
+          still attached
+        </li>
+        <li>It is not missing any accessories or parts</li>
+        <li>
+          You log a return on the Website within 7 days of delivery to you or
+          collection by you of the unwanted product. After 7 days, you can only
+          return a product if it is defective
+        </li>
+      </ul>
+      <p className="mt-4">
+        Want to exchange? If the product you need is not available, we will
+        credit your account with the purchase price of the product within 10
+        days of the return (or refund you if that is your preference). We are
+        entitled to inspect the product to validate your return.
+      </p>
+
+      <h3 className="text-lg font-bold mt-8">Not what you ordered?</h3>
+      <p className="mt-4">
+        If we accidentally deliver the wrong product to you, or if the product
+        is not as described on the Website, please notify us and we will collect
+        the product from you at no charge. If the product is missing any
+        accessories or parts, you will need to follow the process set out in
+        section 2 below. Once we have inspected the product and validated your
+        return, we will, at your choice, deliver the correct product to you as
+        soon as possible (if the correct product is available) or credit your
+        account with the purchase price of the product within 10 days of the
+        return (or refund you if that is your preference).
+      </p>
+
+      <h3 className="text-lg font-bold mt-8">
+        2: Products damaged on delivery
+      </h3>
+      <p className="mt-4">
+        Should a product be damaged or missing any parts or accessories at the
+        time of delivery / collection, please notify us within 7 days of such
+        delivery / collection by logging a return with us. We will arrange to
+        collect the product from you at no charge. Once we have inspected the
+        product and validated your return, we will, at your choice, repair /
+        replace the product as soon as possible (if such repair is possible/ we
+        have the same product in stock to use as a replacement) or credit your
+        account with the purchase price of the product (or refund you if that is
+        your preference).
+      </p>
+
+      <h3 className="text-lg font-bold mt-8">3: Defective products</h3>
+      <p className="mt-4">
+        We do our best to ensure that the products we deliver to you are of a
+        high quality, and in good working order and without defects.
+      </p>
+      <p className="mt-4">
+        What is a defect? A defect is a material imperfection in the manufacture
+        of a product or any characteristic of a product, which makes the product
+        less acceptable than one would reasonably be entitled to expect in the
+        circumstances.
+      </p>
+      <p className="mt-4">
+        The following will NOT be regarded as defects and will not entitle you
+        to a return under this section 3:
+      </p>
+      <ul className="list-disc ml-8 mt-2">
+        <li>Faults resulting from normal wear and tear</li>
+        <li>
+          Damage arising from negligence, user abuse or incorrect usage of the
+          product
+        </li>
+        <li>Damage arising from electrical surges or sea air corrosion</li>
+        <li>
+          Damage arising from a failure to adequately care for the product
+        </li>
+        <li>Damage arising from unauthorized alterations to the product</li>
+        <li>
+          Where the specifications of a product, although accurately described
+          on the Website and generally fit for its intended purpose, do not suit
+          you
+        </li>
+      </ul>
+
+      <h3 className="text-lg font-bold mt-8">Supplier Warranty</h3>
+      <p className="mt-4">
+        The products will have a supplier warranty of 12 months. If such a
+        product turns out to be defective, please notify us as soon as
+        reasonably possible after you become aware of the defect, but in any
+        event within the supplier warranty period after delivery / collection of
+        the product.
+      </p>
+      <p className="mt-4">
+        You can do so by logging a return with us, and we will facilitate your
+        return of the product to the supplier at no charge. Unfortunately, we
+        cannot facilitate returns that fall outside of the supplier warranty
+        period.
+      </p>
+      <p className="mt-4">
+        Please note that the supplier warranty is subject to whatever terms and
+        conditions the supplier or manufacturer may impose. These are usually
+        stated in a brochure or leaflet inside or on the product packaging. It
+        is your responsibility to make yourself aware of any such terms and
+        conditions.
+      </p>
+      <p className="mt-4">
+        It is also important to note that the remedy offered to you is at the
+        supplier or manufacturer’s discretion what remedy it can offer you.
+        Bronscor is under no obligation to provide you with a credit, repair /
+        replacement, as your remedy lies with the supplier or manufacturer.
+      </p>
+
+      <h2 className="text-xl font-bold mt-8">Charges and refunds</h2>
+      <p className="mt-4">
+        If you return a defective product to us, but you fail to return all of
+        the accessories and parts that were sold with that product, we are
+        entitled to (subject to applicable law) refuse the return, or only to
+        replace the item that you did return; or to estimate the value of the
+        missing accessories and parts and to credit or refund you in respect of
+        the returned item only.
+      </p>
+      <p className="mt-4">
+        If you return a product that does not comply with this Policy, you may
+        be liable to reimburse Bronscor for the cost of collecting the product
+        from you and the cost of having the product returned to you.
+      </p>
+      <p className="mt-4">
+        Under no circumstances will donations you make on our Website, or any
+        goodwill credit you may receive from us, be refunded.
+      </p>
+      <p className="mt-4">
+        Please note that we only refund to the payment method that you
+        originally used – i.e. payment by credit card will be refunded to the
+        same credit card, payment by Instant EFT will be refunded to your
+        nominated bank account.
+      </p>
+    </div>
+  );
+};
+
+export default ReturnPolicyPage;
---
+++ app/terms-and-conditions/page.tsx
@@ -0,0 +1,209 @@
+const TermsAndConditions = () => {
+  return (
+    <main className="py-12 container">
+      <h1 className="text-2xl font-bold">TERMS AND CONDITIONS</h1>
+      <h3 className="text-lg font-medium mt-5">1. Introduction</h3>
+      <ol className="px-16 text-sm">
+        <li>
+          1. This website can be accessed at www.bronscorcc.co.za, related
+          mobi-sites and software applications (the “Website”) and is owned and
+          operated by Bronscor (“Bronscor Express”, “we”, “us” and “our”).
+        </li>
+        <li>
+          2. These Website Terms and Conditions (“Terms and Conditions”) govern
+          the ordering, sale and delivery of Goods, and the use of the Website.
+        </li>
+        <li>
+          3. These Terms and Conditions are binding and enforceable against
+          every person that accesses or uses this Website (“you”, “your” or
+          “user”), including without limitation each user who registers as
+          contemplated below (“registered user”). By using the Website and by
+          clicking on the “Register Now” button on the Website, as may be
+          applicable, you acknowledge that you have read and agree to be bound
+          by these Terms and Conditions.
+        </li>
+        <li>
+          4. The Website enables you to shop online for an extensive range of
+          goods including tools and hardware. (“Goods”).
+        </li>
+        <li>
+          5. When you use our Website, notwithstanding your geographic location,
+          you do so in accordance with these Terms and Conditions and provided
+          that any delivery address, including for returns collections, is a
+          delivery address within the borders of the Republic of South Africa.
+        </li>
+      </ol>
+      <h3 className="text-lg font-medium mt-5">2. Important Notice</h3>
+      <ol className="px-16 text-sm">
+        <li>
+          1. These Terms and Conditions apply to users who are consumers for
+          purposes of the Consumer Protection Act, 68 of 2008 (the “CPA”).
+        </li>
+        <li>
+          2. These Terms and Conditions contain provisions that appear in
+          similar text and style to this clause and which –
+          <ol className="px-4 text-xs py-2">
+            <li>
+              1. may limit the risk or liability of Takealot or a third party;
+              and/or
+            </li>
+            <li>2. may create risk or liability for the user; and/or</li>
+            <li>
+              3. may compel the user to indemnify Takealot or a third party;
+              and/or
+            </li>
+            <li>4. serves as an acknowledgement, by the user, of a fact.</li>
+          </ol>
+        </li>
+        <li>
+          3. Your attention is drawn to these Terms and Conditions because they
+          are important and should be carefully noted.
+        </li>
+        <li>
+          4. If there is any provision in these Terms and Conditions that you do
+          not understand, it is your responsibility to ask Bronscor to explain
+          it to you before you accept the Terms and Conditions or continue using
+          the Website.
+        </li>
+        <li>
+          5. Nothing in these Terms and Conditions is intended or must be
+          understood to unlawfully restrict, limit or avoid any right or
+          obligation, as the case may be, created for either you or Bronscor in
+          terms of the CPA.
+        </li>
+        <li>
+          6. Bronscor permits the use of this Website subject to the Terms and
+          Conditions. By using this Website in any way, you shall be deemed to
+          have accepted all the Terms and Conditions unconditionally. You must
+          not use this Website if you do not agree to the Terms and Conditions.
+        </li>
+      </ol>
+      <h3 className="text-lg font-medium mt-5">3. Returns</h3>
+      <ol className="px-16 text-sm">
+        <li>
+          1. Please refer to our Returns Policy for more information about
+          returning products (and related refunds, replacements or repairs). The
+          Returns Policy is incorporated by reference (which means that it forms
+          part of these Terms and Conditions).
+        </li>
+      </ol>
+      <h3 className="text-lg font-medium mt-5">
+        4. Registration and use of the website
+      </h3>
+      <ol className="px-16 text-sm">
+        <li>1. Only registered users may order Goods on the Website.</li>
+        <li>
+          2. To register as a user, you must provide a unique username and
+          password and provide certain information and personal details to
+          Bronscor. You will need to use your unique username and password to
+          access the Website in order to purchase Goods.
+        </li>
+        <li>
+          3. You agree and warrant that your username and password shall: 1. be
+          used for personal use only; and 2. not be disclosed by you to any
+          third party.
+        </li>
+        <li>
+          4. For security purposes you agree to enter the correct username and
+          password whenever ordering Goods, failing which you will be denied
+          access.
+        </li>
+        <li>
+          5. You agree that, once the correct username and password relating to
+          your account have been entered, irrespective of whether the use of the
+          username and password is unauthorised or fraudulent, you will be
+          liable for payment of such order, save where the order is cancelled by
+          you in accordance with these Terms and Conditions.
+        </li>
+        <li>
+          6. You agree to notify Bronscor immediately upon becoming aware of or
+          reasonably suspecting any unauthorised access to or use of your
+          username and password and to take steps to mitigate any resultant loss
+          or harm.
+        </li>
+        <li>
+          7. By using the Website you warrant that you are 18 (eighteen) years
+          of age or older and of full legal capacity. If you are under the age
+          of 18 (eighteen) or if you are not legally permitted to enter into a
+          binding agreement, then you may use the Website only with the
+          involvement and supervision of your parent or legal guardian. If your
+          parent or legal guardian supervises you and gives his/her consent,
+          then such person agrees to be bound to these Terms and Conditions and
+          to be liable and responsible for you and all your obligations under
+          these Terms and Conditions.
+        </li>
+        <li>
+          8. You agree that you will not in any way use any device, software or
+          other instrument to interfere or attempt to interfere with the proper
+          working of the Website. In addition, you agree that you will not in
+          any way use any robot, spider, other automatic device, or manual
+          process to monitor, copy, distribute or modify the Website or the
+          information contained herein, without the prior written consent from
+          an authorised Bronscor representative (such consent is deemed given
+          for standard search engine technology employed by Internet search
+          websites to direct Internet users to this Website).
+        </li>
+        <li>
+          9. You may not use the Website to distribute material which is
+          defamatory, offensive, contains or amounts to hate speech or is
+          otherwise unlawful.
+        </li>
+        <li>
+          10. You may not in any way display, publish, copy, print, post or
+          otherwise use the Website and/or the information contained therein
+          without the express prior written consent of an authorised Bronscor
+          representative.
+        </li>
+      </ol>
+      <h3 className="text-lg font-medium mt-5">
+        5. Conclusion of sales and availability of stock
+      </h3>
+      <ol className="px-16 text-sm">
+        <li>
+          1. Registered users may place orders for Goods, which Bronscor may
+          accept or reject. Whether or not Bronscor accepts an order depends on
+          the availability of Goods, correctness of the information relating to
+          the Goods (including without limitation the price) and receipt of
+          payment or payment authorisation by Bronscor for the Goods.
+        </li>
+        <li>
+          2. NOTE: Bronscor will indicate the acceptance of your order by
+          delivering the Goods to you or allowing you to collect them, and only
+          at that point will an agreement of sale between you and Bronscor come
+          into effect (the “Sale”). This is regardless of any communication from
+          Bronscor stating that your order or payment has been confirmed.
+          Bronscor will indicate the rejection of your order (by Bronscor
+          itself) by cancelling it and, as soon as possible thereafter,
+          refunding you for any amount already paid.
+        </li>
+        <li>
+          3. Prior to delivery or your collection of the Goods, you may cancel
+          an order at any time provided you do so before receiving a dispatch or
+          delivery notice. After delivery or your collection of the Goods, you
+          may return the Goods only in accordance with the Returns Policy.
+        </li>
+        <li>
+          4. Placing Goods in a wishlist or shopping basket without completing
+          the purchase cycle does not constitute an order for such Goods, and as
+          such, Goods may be removed from the shopping basket if stock is no
+          longer available or the price thereof might change without notice to
+          you. You cannot hold Bronscor liable if such Goods are not available
+          or are not available at the particular price when you complete or
+          attempt to complete the purchase cycle at a later stage.
+        </li>
+        <li>
+          5. You acknowledge that stock of all Goods on offer is limited and
+          that pricing may change at any time without notice to you. In the case
+          of Goods for sale by Bronscor, Bronscor will take all reasonable
+          efforts to monitor stock levels and ensure that when stock is no
+          longer available, that offers thereof are discontinued on the Website.
+          However, we cannot guarantee the availability of stock. When Goods are
+          no longer available after you have placed an order, Bronscor will
+          notify you and you will be entitled to a refund of any amount already
+          paid by you for such Goods.
+        </li>
+      </ol>
+    </main>
+  );
+};
+export default TermsAndConditions;
