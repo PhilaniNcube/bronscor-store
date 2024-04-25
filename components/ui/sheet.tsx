c736eafd2dbc5d2de409ca4345da808324b3d0e5
Philani Ncube <ncbphi001@gmail.com>
2024-04-25 23:19:01 +0200

    Fix className prop in SheetPortal component

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -31,11 +31,13 @@ interface SheetPortalProps
 
 const SheetPortal = ({
   position,
-  className,
+  // className,
   children,
   ...props
 }: SheetPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props}>
+  <SheetPrimitive.Portal
+  // className={cn(className)}
+  {...props}>
     <div className={portalVariants({ position })}>{children}</div>
   </SheetPrimitive.Portal>
 )
@@ -161,7 +163,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4 bg-red-600 text-white" />
+        <X className="w-4 h-4 text-white bg-red-600" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
