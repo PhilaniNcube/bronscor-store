3b9b0c585644a16207a7f77e46fbf46207892ca3
Philani Ncube <ncbphi001@gmail.com>
2024-06-13 20:53:37 +0200

    refactor: Add Supabase authentication check to forgot password page

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,6 +1,18 @@
+import { createClient } from "@/utils/supabase/server";
 import ForgotPassword from "./_components/ForgotPasswordForm";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+
+   const supabase = createClient();
+
+  // get current user
+  const {data: {user}} = await supabase.auth.getUser();
+
+  if(user !== null) {
+    redirect("/");
+  }
+
   return <div className="flex items-center justify-center min-h-[80vh]">
     <ForgotPassword />
   </div>;
